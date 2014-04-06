a2c3325bf47b2698ed74d04d30b29c9240bb3a43
shelleyhope <shelleyandkenny@mac.com>
2014-04-06 14:02:57 +0100

    comments added

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
 puts "The students of my cohort at Makers academy"
 puts "-------------------------------------------"
+#first we print the list of students:
 puts "Steve Musgrave"
 puts "Sroop Sunar"
 puts "Colin Marshall"
@@ -19,8 +20,9 @@ puts "Robert Leon"
 puts "Emma Williams"
 puts "Joey Wolf"
 puts "Julie Walker"
-
+#finally we print the total:
 print "Overall, we have "
+#it's important that print doesn't add newline characters.
 print 19
 puts " great students."
 
