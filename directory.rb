942bd0006712b3226846670fa470652343a31502
shelleyhope <shelleyandkenny@mac.com>
2014-04-09 10:36:51 +0100

    added student names

---
+++ directory.rb
@@ -0,0 +1,11 @@
+puts "The Students of my cohort at Makers Academy"
+puts "-------------------------------------------"	
+puts "David Beckham"
+puts "David Tennant"
+puts "Vincent Gallo"
+puts "Colin Firth"
+puts "Johnny Depp"
+puts "Dan Ariely"
+print "Overall, we have "
+print 6
+puts " great students"
