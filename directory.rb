a6e076a09c491192ee95a779ea87d09c14ce4dd9
shelleyhope <shelleyandkenny@mac.com>
2014-04-09 11:43:33 +0100

    amended to ask for user input

--- directory.rb
+++ directory.rb
@@ -1,32 +1,37 @@
-student_count = 6
-
-#lets put all the students into an array:
-students = [
-{:name=> "David Beckham", :cohort => :march},
-{:name=> "David Tennant", :cohort => :march},
-{:name=> "Vincent Gallo", :cohort => :march},
-{:name=> "Colin Firth", :cohort => :march},
-{:name=> "Johnny Depp", :cohort => :march},
-{:name=> "Dan Ariely", :cohort => :march}
-]
-
 def print_header
-	puts "The Students of my cohort at Makers Academy"
-	puts "-------------------------------------------"	
+	puts "The students of my cohort:"
+	puts "--------------------------"
 end
 
-def print(students)
-students.each do |student|
-	puts "#{student[:name]} (#{student[:cohort]} cohort)"
-end
+def print_footer(names)
+	puts "Overall, we have #{names.length} great students"
 end
 
-def print_footer(array)
-puts "Overall, we have #{array.length} great students"
+def input_students
+	puts "Please enter the names of the students:"
+	puts "To finish, just hit return twice."
+	
+	#create an empty array:
+	students = []
+
+	#get the first name:
+	name = gets.chomp
+	
+	#while the name is not empty, repeat this code:
+	while !name.empty? do
+		#add the student hash to the array
+		students << {:name => name, :cohort => :march}
+
+		puts "Now we have #{students.length} students. Please enter the next student name:"
+
+		#get another name from the user:
+		name = gets.chomp
+	end
+	#return the array of students
+	students
 end
 
-#nothing happens until we call the methods:
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
